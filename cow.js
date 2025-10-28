0c2271b184bf1e00f90e6a65aa62e162d3e96e7a
BIG-BOSS <bananaman080604@gmail.com>
2025-10-28 17:56:48 -0600

    Refactor: Separate HTML, CSS, and JS into distinct files

---
+++ cow.js
@@ -0,0 +1,10 @@
+// --- UI Selectors ---
+const runtimeDisplay = document.getElementById('runtime-display'), fpsDisplay = document.getElementById('fps-display');
+const targetXDisplay = document.getElementById('target-x'), targetYDisplay = document.getElementById('target-y'), targetZDisplay = document.getElementById('target-z');
+const interceptorYDisplay = document.getElementById('interceptor-y');
+const turretAzimuthDisplay = document.getElementById('turret-azimuth'), turretElevationDisplay = document.getElementById('turret-elevation');
+const statusDisplay = document.getElementById('status-display');
+const simulationScreen = document.getElementById('simulation-screen');
+
+const TARGET_ART = ["( ͡° ͜ʖ ͡°)"]
+const WRECKAGE_ART = ["#"];
